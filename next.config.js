06b7850e9e7812d48971523210f42b91f414fc26
mihascor <mihascorkiz@gmail.com>
2026-07-12 13:59:05 +0300

    Добавил поддержку меток языка в блоках кода и обновил стили для их отображения; добавил статью о HTML-каркасе страницы

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
+  mdxOptions: {
+    rehypePlugins: [rehypeCodeLanguageLabel],
+  },
 })
 
 module.exports = withNextra()
+
+function rehypeCodeLanguageLabel() {
+  return (tree) => {
+    visit(tree, (node) => {
+      if (node.type !== 'element' || node.tagName !== 'pre') {
+        return
+      }
+
+      const code = node.children?.find(
+        (child) => child.type === 'element' && child.tagName === 'code',
+      )
+      const language = code?.properties?.className?.find((className) =>
+        className.startsWith('language-'),
+      )
+
+      if (language && language !== 'language-text') {
+        node.properties = {
+          ...node.properties,
+          'data-language': language.replace('language-', ''),
+        }
+      }
+    })
+  }
+}
+
+function visit(node, callback) {
+  callback(node)
+
+  for (const child of node.children ?? []) {
+    visit(child, callback)
+  }
+}
